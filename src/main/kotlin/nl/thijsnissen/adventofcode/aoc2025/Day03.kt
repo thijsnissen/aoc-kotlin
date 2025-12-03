0963c7c9d436d0604d013b7dc2e85b421dcb4231
Thijs Nissen <thijsnissen@gmail.com>
2025-12-03 19:53:45 +0100

    solved 202503

--- src/main/kotlin/nl/thijsnissen/adventofcode/aoc2025/Day03.kt
+++ src/main/kotlin/nl/thijsnissen/adventofcode/aoc2025/Day03.kt
@@ -2,12 +2,20 @@ package nl.thijsnissen.adventofcode.aoc2025
 
 import nl.thijsnissen.adventofcode.AdventOfCode
 
-object Day03 : AdventOfCode<Int, Int>("test") {
-    val day03 = input()
+object Day03 : AdventOfCode<Long, Long>("input") {
+    val banks: List<List<Int>> = input().lines().map { it.map { it.digitToInt() } }
 
-    override fun part1(): Int = 0
+    tailrec fun List<Int>.maxJoltage(n: Int, acc: String = ""): Long =
+        if (n == 0) acc.toLong()
+        else {
+            val (i, m) = dropLast(n - 1).withIndex().maxBy { it.value }
 
-    override fun part2(): Int = 0
+            drop(i + 1).maxJoltage(n - 1, acc + m)
+        }
+
+    override fun part1(): Long = banks.sumOf { it.maxJoltage(2) }
+
+    override fun part2(): Long = banks.sumOf { it.maxJoltage(12) }
 }
 
 fun main() {
---
+++ src/main/kotlin/nl/thijsnissen/adventofcode/aoc2025/Day04.kt
@@ -0,0 +1,15 @@
+package nl.thijsnissen.adventofcode.aoc2025
+
+import nl.thijsnissen.adventofcode.AdventOfCode
+
+object Day04 : AdventOfCode<Int, Int>("test") {
+    val day04 = input()
+
+    override fun part1(): Int = 0
+
+    override fun part2(): Int = 0
+}
+
+fun main() {
+    Day04.solve()
+}
