8ebf3cd67a7d75f0719fe6299c73d60463e19ce9
Thijs Nissen <thijsnissen@gmail.com>
2025-12-07 08:43:08 +0100

    more details

--- src/main/kotlin/nl/thijsnissen/adventofcode/aoc2025/Day06.kt
+++ src/main/kotlin/nl/thijsnissen/adventofcode/aoc2025/Day06.kt
@@ -32,26 +32,26 @@ object Day06 : AdventOfCode<Long, Long>("input") {
             fun forCephalopodMath(ss: List<String>): List<Problem> {
                 val numbers =
                     ss.dropLast(1)
-                        .map { it.toList().padEnd(ss.maxOf { it.length }, ' ') }
+                        .map { it.toList().padEnd(ss.maxOf(String::length), ' ') }
                         .transpose()
                         .fold(listOf(emptyList<Long>())) { acc, n ->
-                            if (n.all { it.isWhitespace() }) acc.plusElement(emptyList())
+                            if (n.all(Char::isWhitespace)) acc.plusElement(emptyList())
                             else
                                 acc.dropLast(1)
                                     .plusElement(
                                         acc.last().plusElement(n.joinToString("").trim().toLong())
                                     )
                         }
-                val operations = ss.last().filterNot { it.isWhitespace() }.toList()
+                val operations = ss.last().filterNot(Char::isWhitespace).toList()
 
                 return numbers.zip(operations).map(Problem::fromPair)
             }
         }
     }
 
-    override fun part1(): Long = Problem.forNormalMath(worksheet).sumOf { it.solve() }
+    override fun part1(): Long = Problem.forNormalMath(worksheet).sumOf(Problem::solve)
 
-    override fun part2(): Long = Problem.forCephalopodMath(worksheet).sumOf { it.solve() }
+    override fun part2(): Long = Problem.forCephalopodMath(worksheet).sumOf(Problem::solve)
 }
 
 fun main() {
