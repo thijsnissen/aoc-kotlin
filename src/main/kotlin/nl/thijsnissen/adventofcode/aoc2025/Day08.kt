cee1c05285bf1291f5a93f5c0041f47745e8f5ef
Thijs Nissen <thijsnissen@gmail.com>
2025-12-09 00:30:10 +0100

    use Int instead of Long in 2025 day 8

--- src/main/kotlin/nl/thijsnissen/adventofcode/aoc2025/Day08.kt
+++ src/main/kotlin/nl/thijsnissen/adventofcode/aoc2025/Day08.kt
@@ -4,46 +4,46 @@ import kotlin.math.pow
 import kotlin.math.sqrt
 import nl.thijsnissen.adventofcode.AdventOfCode
 
-object Day08 : AdventOfCode<Long, Long>("input") {
-    val boxes: List<Pos> = input().lines().map { Pos.fromString(it) }
+object Day08 : AdventOfCode<Int, Int>("input") {
+    val boxes: List<Box> = input().lines().map { Box.fromString(it) }
 
-    data class Pos(val x: Long, val y: Long, val z: Long) {
-        fun euclidean(that: Pos): Double =
+    data class Box(val x: Int, val y: Int, val z: Int) {
+        fun euclidean(that: Box): Double =
             sqrt(listOf(x - that.x, y - that.y, z - that.z).sumOf { it.toDouble().pow(2) })
 
         companion object {
-            fun fromString(s: String): Pos =
-                s.split(",").let { Pos(it[0].toLong(), it[1].toLong(), it[2].toLong()) }
+            fun fromString(s: String): Box =
+                s.split(",").let { Box(it[0].toInt(), it[1].toInt(), it[2].toInt()) }
         }
     }
 
-    fun Set<Set<Pos>>.find(p: Pos): Set<Pos>? = find { it.contains(p) }
+    fun Set<Set<Box>>.find(b: Box): Set<Box>? = find { it.contains(b) }
 
-    fun Set<Set<Pos>>.union(a: Set<Pos>, b: Set<Pos>) =
+    fun Set<Set<Box>>.union(a: Set<Box>, b: Set<Box>) =
         minusElement(a).minusElement(b).plusElement(a + b)
 
-    data class State(val pairs: List<Pair<Pos, Pos>>, val acc: Set<Set<Pos>> = emptySet()) {
+    data class State(val ps: List<Pair<Box, Box>>, val acc: Set<Set<Box>> = emptySet()) {
         fun makeConnection(): State {
-            val (a, b) = pairs.first()
+            val (a, b) = ps.first()
             val sa = acc.find(a) ?: setOf(a)
             val sb = acc.find(b) ?: setOf(b)
 
-            return State(pairs.drop(1), acc.union(sa, sb))
+            return State(ps.drop(1), acc.union(sa, sb))
         }
 
         fun makeConnections(n: Int): State =
             generateSequence(this) { it.makeConnection() }.drop(n).first()
 
-        fun multiply(maxN: Int): Long =
-            acc.map { it.size.toLong() }.sorted().takeLast(maxN).reduce { acc, s -> acc * s }
+        fun multiply(maxN: Int): Int =
+            acc.map { it.size }.sorted().takeLast(maxN).reduce { acc, s -> acc * s }
 
-        fun connectAll(n: Int): Pair<Pos, Pos> =
+        fun connectAll(n: Int): Pair<Box, Box> =
             makeConnection().let {
-                if (it.acc.firstOrNull()?.size == n) pairs.first() else it.connectAll(n)
+                if (it.acc.firstOrNull()?.size == n) ps.first() else it.connectAll(n)
             }
 
         companion object {
-            fun fromBoxes(bs: List<Pos>): State =
+            fun fromBoxes(bs: List<Box>): State =
                 State(
                     bs.flatMapIndexed { i, a -> boxes.drop(i + 1).map { b -> a to b } }
                         .sortedBy { it.first.euclidean(it.second) }
@@ -51,10 +51,10 @@ object Day08 : AdventOfCode<Long, Long>("input") {
         }
     }
 
-    override fun part1(): Long =
+    override fun part1(): Int =
         State.fromBoxes(boxes).makeConnections(if (getExt() == "test") 10 else 1000).multiply(3)
 
-    override fun part2(): Long =
+    override fun part2(): Int =
         State.fromBoxes(boxes).connectAll(boxes.size).let { it.first.x * it.second.x }
 }
 
