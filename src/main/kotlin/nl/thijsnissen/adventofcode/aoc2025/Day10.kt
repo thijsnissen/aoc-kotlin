d1a22462b9ab4bbcbc5eed6cb3a693222bf78b91
Thijs Nissen <thijsnissen@gmail.com>
2025-12-10 12:49:48 +0100

    adds 2025 day 10 template

---
+++ src/main/kotlin/nl/thijsnissen/adventofcode/aoc2025/Day10.kt
@@ -0,0 +1,15 @@
+package nl.thijsnissen.adventofcode.aoc2025
+
+import nl.thijsnissen.adventofcode.AdventOfCode
+
+object Day10 : AdventOfCode<Int, Int>("input") {
+    val day10 = input()
+
+    override fun part1(): Int = 0
+
+    override fun part2(): Int = 0
+}
+
+fun main() {
+    Day10.solve()
+}
