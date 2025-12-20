41fe1003616f4d7d421bb668c40413134fc8fd15
Thijs Nissen <thijsnissen@gmail.com>
2025-12-21 00:00:01 +0100

    solves 2025 day 10 part 2

--- src/main/kotlin/nl/thijsnissen/adventofcode/aoc2025/Day10.kt
+++ src/main/kotlin/nl/thijsnissen/adventofcode/aoc2025/Day10.kt
@@ -6,25 +6,44 @@ object Day10 : AdventOfCode<Int, Int>("input") {
     val machines: List<Machine> = input().lines().map { Machine.fromString(it) }
 
     data class Machine(val target: Int, val buttons: List<List<Int>>, val joltages: List<Int>) {
-        fun configure(): Int {
-            tailrec fun loop(todo: List<Pair<Int, Int>>, seen: Set<Int>): Int {
-                val (s, c) = todo.first()
+        fun configureIndicatorLights(): Int =
+            buttons
+                .combinations()
+                .filter { target == it.fold(0) { acc, b -> acc xor b.toBitMask() } }
+                .minOf { it.size }
 
-                return when (s) {
-                    target -> c
-                    in seen -> loop(todo.drop(1), seen)
-                    else ->
-                        loop(
-                            todo.drop(1) + buttons.map { (s xor it.toBitMask()) to (c + 1) },
-                            seen + s,
-                        )
+        fun configureCounters(): Int {
+            val c = mutableMapOf<List<Int>, Int?>()
+
+            fun loop(js: List<Int>): Int? =
+                c.getOrPut(js) {
+                    if (js.all { it == 0 }) 0
+                    else {
+                        patterns.entries.fold(null) { acc, (p, s) ->
+                            // Pattern should not exceed target and should match odd/even parity
+                            if (!p.indices.all { p[it] <= js[it] && p[it] % 2 == js[it] % 2 }) acc
+                            else
+                                loop(js.mapIndexed { i, j -> (j - p[i]) / 2 })?.let { n ->
+                                    minOf(acc ?: Int.MAX_VALUE, s + 2 * n)
+                                } ?: acc
+                        }
+                    }
                 }
-            }
 
-            return loop(listOf(0 to 0), setOf())
+            return loop(joltages) ?: error("unable to configure joltage level counters: $joltages")
         }
 
-        fun increase(): Int = 0 // TODO
+        private val patterns: Map<List<Int>, Int> by lazy {
+            buttons
+                .combinations()
+                .map { bs ->
+                    val indices = bs.flatten()
+
+                    joltages.indices.map { i -> indices.count { i == it } } to bs.size
+                }
+                .groupBy({ it.first }, { it.second })
+                .mapValues { it.value.min() }
+        }
 
         companion object {
             fun empty(): Machine = Machine(0, emptyList(), emptyList())
@@ -34,30 +53,48 @@ object Day10 : AdventOfCode<Int, Int>("input") {
                     when {
                         s.startsWith("[") -> acc.copy(target = s.toBitMask())
                         s.startsWith("(") ->
-                            acc.copy(buttons = acc.buttons.plusElement(toButton(s)))
-                        s.startsWith("{") -> acc.copy(joltages = toJoltages(s))
+                            acc.copy(buttons = acc.buttons.plusElement(s.toButton()))
+                        s.startsWith("{") -> acc.copy(joltages = s.toJoltages())
                         else -> acc
                     }
                 }
 
-            fun toButton(s: String): List<Int> =
-                s.substring(1, s.length - 1).split(",").map { it.toInt() }
+            fun String.toButton(): List<Int> =
+                removeSurrounding("(", ")").split(",").map { it.toInt() }
+
+            fun String.toJoltages(): List<Int> =
+                removeSurrounding("{", "}").split(",").map { it.toInt() }
 
             fun List<Int>.toBitMask(): Int = fold(0) { acc, i -> acc or (1 shl i) }
 
             fun String.toBitMask(): Int =
-                substring(1, length - 1).foldIndexed(0) { i, acc, c ->
+                removeSurrounding("[", "]").foldIndexed(0) { i, acc, c ->
                     if (c == '#') acc + (1 shl i) else acc
                 }
-
-            fun toJoltages(s: String): List<Int> =
-                s.substring(1, s.length - 1).split(",").map { it.toInt() }
         }
     }
 
-    override fun part1(): Int = machines.sumOf { it.configure() }
+    fun <T> List<T>.combinations(): List<List<T>> = (0..size).flatMap { combinations(it) }
 
-    override fun part2(): Int = machines.sumOf { it.increase() }
+    fun <T> List<T>.combinations(size: Int): List<List<T>> =
+        when {
+            size == 0 -> listOf(emptyList())
+            isEmpty() -> emptyList()
+            else -> {
+                val h = listOf(first())
+                val t = drop(1)
+
+                t.combinations(size - 1).map { h + it } + t.combinations(size)
+            }
+        }
+
+    override fun part1(): Int = machines.sumOf { it.configureIndicatorLights() }
+
+    /**
+     * Solved part 2 using the wonderful solution proposed by u/tenthmascot at:
+     * https://www.reddit.com/r/adventofcode/comments/1pk87hl/2025_day_10_part_2_bifurcate_your_way_to_victory/
+     */
+    override fun part2(): Int = machines.sumOf { it.configureCounters() }
 }
 
 fun main() {
--- src/test/kotlin/nl/thijsnissen/adventofcode/aoc2025/Aoc2025Test.kt
+++ src/test/kotlin/nl/thijsnissen/adventofcode/aoc2025/Aoc2025Test.kt
@@ -125,12 +125,12 @@ object Aoc2025Test {
     fun `Day 10 - Factory`() {
         if (Day10.getExt() == "test") {
             Assertions.assertEquals(7, Day10.part1())
-            Assertions.assertEquals(0, Day10.part2())
+            Assertions.assertEquals(33, Day10.part2())
         }
 
         if (Day10.getExt() == "input") {
             Assertions.assertEquals(449, Day10.part1())
-            Assertions.assertEquals(0, Day10.part2())
+            Assertions.assertEquals(17848, Day10.part2())
         }
     }
 
